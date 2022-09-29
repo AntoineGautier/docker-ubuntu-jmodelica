359dfb95b6ebd4b8f6797462b014b6ddc6b58a49
AntoineGautier <agautier@solamen.fr>
2022-09-29 10:08:16 +0200

    Add modelicapath and do not generate HTML diag

--- jmodelica.py
+++ jmodelica.py
@@ -5,83 +5,99 @@
 ##########################################################################
 # Import the function for compilation of models and the load_fmu method
 
-from pymodelica import compile_fmu
-import traceback
-import logging
-
-from pyfmi import load_fmu
-import pymodelica
-
+import matplotlib.pyplot as plt
 import os
 import shutil
 import sys
-import matplotlib.pyplot as plt
+
+import pymodelica
+
+from inspect import signature
+
+from pyfmi import load_fmu
+from pymodelica import compile_fmu
 
 debug_solver = False
-model="Buildings.Utilities.Psychrometrics.Examples.DewPointTemperature"
+model = "Buildings.Utilities.Psychrometrics.Examples.DewPointTemperature"
 generate_plot = False
 
 # Overwrite model with command line argument if specified
 if len(sys.argv) > 1:
-  # If the argument is a file, then parse it to a model name
-  if os.path.isfile(sys.argv[1]):
-    model = sys.argv[1].replace(os.path.sep, '.')[:-3]
-  else:
-    model=sys.argv[1]
-
+    # If the argument is a file, then parse it to a model name
+    if os.path.isfile(sys.argv[1]):
+        model = sys.argv[1].replace(os.path.sep, '.')[:-3]
+    else:
+        model = sys.argv[1]
 
 print("*** Compiling {}".format(model))
 # Increase memory
 pymodelica.environ['JVM_ARGS'] = '-Xmx4096m'
 
-
 sys.stdout.flush()
 
 ######################################################################
 # Compile fmu
-fmu_name = compile_fmu(model,
-                       version="2.0",
-                       compiler_log_level='warning', #'info', 'warning',
-                       compiler_options = {"generate_html_diagnostics" : True,
-                                           "nle_solver_tol_factor": 1e-2}) # 1e-2 is the default
+kwargs = dict(
+    version='2.0',
+    compiler_log_level='warning',  # 'info', 'warning'
+    compiler_options={
+        "generate_html_diagnostics": False,
+        "nle_solver_tol_factor": 1e-2,  # 1e-2 is the default
+    },
+)
+
+if 'modelicapath' in str(signature(compile_fmu)):
+    # This is the new API that uses modelicapath as a function argument.
+
+    removed_modelica_path = False
+    if 'MODELICAPATH' in os.environ:
+        modelicapath = os.environ['MODELICAPATH']
+        del os.environ['MODELICAPATH']
+        removed_modelica_path = True
+    else:
+        modelicapath = os.path.abspath('.')
+
+    kwargs['modelicapath'] = modelicapath
+
+fmu_name = compile_fmu(model, **kwargs)
 
 ######################################################################
 # Load model
-mod = load_fmu(fmu_name, log_level=4) # default setting is 3
-mod.set_max_log_size(2073741824) # = 2*1024^3 (about 2GB)
+mod = load_fmu(fmu_name, log_level=3)  # 4 # default setting is 3
+mod.set_max_log_size(2073741824)  # = 2*1024^3 (about 2GB)
 ######################################################################
 # Retrieve and set solver options
 x_nominal = mod.nominal_continuous_states
-opts = mod.simulate_options() #Retrieve the default options
+opts = mod.simulate_options()  #Retrieve the default options
 
-opts['solver'] = 'CVode' #'Radau5ODE' #CVode
+opts['solver'] = 'CVode'  #'Radau5ODE' #CVode
 opts['ncp'] = 500
 
 if opts['solver'].lower() == 'cvode':
-  # Set user-specified tolerance if it is smaller than the tolerance in the .mo file
-  rtol = 1.0e-8
-  x_nominal = mod.nominal_continuous_states
+    # Set user-specified tolerance if it is smaller than the tolerance in the .mo file
+    rtol = 1.0e-8
+    x_nominal = mod.nominal_continuous_states
 
-  if len(x_nominal) > 0:
-    atol = rtol*x_nominal
-  else:
-    atol = rtol
+    if len(x_nominal) > 0:
+        atol = rtol * x_nominal
+    else:
+        atol = rtol
 
-  opts['CVode_options']['external_event_detection'] = False
-  opts['CVode_options']['maxh'] = (mod.get_default_experiment_stop_time()-mod.get_default_experiment_start_time())/float(opts['ncp'])
-  opts['CVode_options']['iter'] = 'Newton'
-  opts['CVode_options']['discr'] = 'BDF'
-  opts['CVode_options']['rtol'] = rtol
-  opts['CVode_options']['atol'] = atol
-  opts['CVode_options']['store_event_points'] = True # True is default, set to false if many events
+    opts['CVode_options']['external_event_detection'] = False
+    opts['CVode_options']['maxh'] = (mod.get_default_experiment_stop_time() -
+                                     mod.get_default_experiment_start_time()) / float(opts['ncp'])
+    opts['CVode_options']['iter'] = 'Newton'
+    opts['CVode_options']['discr'] = 'BDF'
+    opts['CVode_options']['rtol'] = rtol
+    opts['CVode_options']['atol'] = atol
+    opts['CVode_options']['store_event_points'] = True  # True is default, set to false if many events
 
-  if debug_solver:
-    opts['CVode_options']['clock_step'] = True
-
+    if debug_solver:
+        opts['CVode_options']['clock_step'] = True
 
 if debug_solver:
-  opts["logging"] = True #<- Turn on solver debug logging
-  mod.set("_log_level", 4)
+    opts["logging"] = True  #<- Turn on solver debug logging
+    mod.set("_log_level", 4)
 
 ######################################################################
 # Simulate
@@ -89,12 +105,12 @@ res = mod.simulate(options=opts)
 #        logging.error(traceback.format_exc())
 
 if generate_plot:
-  plt.plot(res['time'], res['TDewPoi.T']-273.15)
-  plt.xlabel('time in [s]')
-  plt.ylabel('Dew point [degC]')
-  plt.grid()
-  plt.show()
-  plt.savefig("plot.pdf")
+    plt.plot(res['time'], res['TDewPoi.T'] - 273.15)
+    plt.xlabel('time in [s]')
+    plt.ylabel('Dew point [degC]')
+    plt.grid()
+    plt.show()
+    plt.savefig("plot.pdf")
 
 ######################################################################
 # Copy style sheets.
@@ -110,29 +126,27 @@ if os.path.exists(htm_dir):
 ######################################################################
 # Get debugging information
 if debug_solver:
-  #Load the debug information
-  from pyfmi.debug import CVodeDebugInformation
-  debug = CVodeDebugInformation(model.replace(".", "_")+"_debug.txt")
+    #Load the debug information
+    from pyfmi.debug import CVodeDebugInformation
+    debug = CVodeDebugInformation(model.replace(".", "_") + "_debug.txt")
 
-  ### Below are options to plot the order, error and step-size evolution.
-  ### The error methos also take a threshold and a region if you want to
-  ### limit the plot to a certain interval.
+    ### Below are options to plot the order, error and step-size evolution.
+    ### The error methos also take a threshold and a region if you want to
+    ### limit the plot to a certain interval.
 
+    if opts['solver'].lower() == 'cvode':
+        #Plot wall-clock time versus model time
+        debug.plot_cumulative_time_elapsed()
+        #Plot only the region 0.8 - 1.0 seconds and only state variables with an error greater than 0.01 (for any point in that region)
+        debug.plot_error(region=[0.8, 1.0], threshold=0.01)
 
-  if opts['solver'].lower() == 'cvode':
-    #Plot wall-clock time versus model time
-    debug.plot_cumulative_time_elapsed()
-    #Plot only the region 0.8 - 1.0 seconds and only state variables with an error greater than 0.01 (for any point in that region)
-    debug.plot_error(region=[0.8,1.0], threshold=0.01)
+    #Plot order evolution
+    debug.plot_order()
 
+    #Plot error evolution
+    debug.plot_error()  #Note see also the arguments to the method
 
-  #Plot order evolution
-  debug.plot_order()
+    #Plot the used step-size
+    debug.plot_step_size()
 
-  #Plot error evolution
-  debug.plot_error() #Note see also the arguments to the method
-
-  #Plot the used step-size
-  debug.plot_step_size()
-
-  #See also debug?
+    #See also debug?
